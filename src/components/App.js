09254d484b767c9aa5a17fddaf3ed382bf7fd8a2
theMyth333 <mithunkarthick3@gmail.com>
2023-12-10 01:35:43 +0530

    ++ postings table

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   ThemeProvider,
   Form,
@@ -9,53 +9,32 @@ import {
   Button,
   ButtonType,
   DatePicker,
-  Label
-} from '@ui5/webcomponents-react';
+  Label,
+  Table,
+  TableColumn,
+  TableRow,
+  TableCell,
+} from "@ui5/webcomponents-react";
 //For the submits property of the button to have effect, you must add the following import
-import '@ui5/webcomponents/dist/features/InputElementsFormSupport.js';
+import "@ui5/webcomponents/dist/features/InputElementsFormSupport.js";
 
 export default function RegisterForm() {
-  const postings = {
-    Ledger: '0L',
-    companyCode: '1710',
-    account: '1001000',
-    PostingDate: new Date(),
-    Amount: 100
-  };
-
-  const [ledger, setLedger] = useState('0L');
-  const [companyCode, setCompanyCode] = useState('1710');
-  const [account, setAccount] = useState('1001000');
-  const [postingDate, setPostingDate] = useState('');
-  const [amount, setAmount] = useState('100');
+  const [formData, setFormData] = useState({
+    ledger: "2L",
+    companyCode: "1710",
+    account: "1001000",
+    postingDate: "",
+    amount: 100,
+  });
+  const [tableData, setTableData] = useState([]);
 
   const handleSubmit = (e) => {
-    const values = {
-      ledger: ledger,
-      companyCode: companyCode,
-      account: account,
-      postingDate: postingDate,
-      amount: amount
-    };
-    alert(JSON.stringify(values, null, 2));
-    console.log(values);
-    //prevent page reload
+    setTableData([...tableData, { ...formData }]);
     e.preventDefault();
   };
-  const handleLedgerInput = (e) => {
-    setLedger(e.target.value);
-  };
-  const handleCompanyCodeInput = (e) => {
-    setCompanyCode(e.target.value);
-  };
-  const handleAccountInput = (e) => {
-    setAccount(e.target.value);
-  };
-  const handlePostingDateChange = (e) => {
-    setPostingDate(e.detail.value);
-  };
-  const handleAmountInput = (e) => {
-    setAmount(e.target.value);
+
+  const handleChange = (event, field) => {
+    setFormData({ ...formData, [field]: event.target.value });
   };
 
   return (
@@ -63,25 +42,86 @@ export default function RegisterForm() {
       <Form onSubmit={handleSubmit}>
         <FormGroup titleText="Simulate GL Postings">
           <FormItem label={<Label>Ledger</Label>}>
-            <Input name="ledger" type={InputType.Text } value={ledger} onInput={handleLedgerInput} />
+            <Input
+              name="ledger"
+              type={InputType.Text}
+              value={formData.ledger}
+              onInput={(e) => handleChange(e, "ledger")}
+            />
           </FormItem>
           <FormItem label={<Label>Company Code</Label>}>
-            <Input name="companyCode" type={InputType.Number} value={companyCode} onInput={handleCompanyCodeInput} />
+            <Input
+              name="companyCode"
+              type={InputType.Number}
+              value={formData.companyCode}
+              onInput={(e) => handleChange(e, "companyCode")}
+            />
           </FormItem>
           <FormItem label="GL Account">
-          <Input name="account" type={InputType.Number} value={account} onInput={handleAccountInput} />
+            <Input
+              name="account"
+              type={InputType.Number}
+              value={formData.account}
+              onInput={(e) => handleChange(e, "account")}
+            />
           </FormItem>
           <FormItem label="Posting Date">
-            <DatePicker value={postingDate} onChange={handlePostingDateChange}></DatePicker>
+            <DatePicker
+              value={formData.postingDate}
+              onChange={(e) => handleChange(e, "postingDate")}
+            ></DatePicker>
           </FormItem>
           <FormItem label="Amount">
-          <Input name="amount" type={InputType.Number} value={amount} onInput={handleAmountInput} />
+            <Input
+              name="amount"
+              type={InputType.Number}
+              value={formData.amount}
+              onInput={(e) => handleChange(e, "amount")}
+            />
           </FormItem>
           <FormItem>
-            <Button type={ButtonType.Submit}>Submit</Button>
+            <Button type={ButtonType.Submit}>Post</Button>
           </FormItem>
         </FormGroup>
       </Form>
+      <Table>
+        <TableColumn>
+          <Label>Ledger</Label>
+        </TableColumn>
+        <TableColumn>
+          <Label>Company Code</Label>
+        </TableColumn>
+        <TableColumn>
+          <Label>GL Account</Label>
+        </TableColumn>
+        <TableColumn>
+          <Label>Posting Date</Label>
+        </TableColumn>
+        <TableColumn>
+          <Label>Amount</Label>
+        </TableColumn>
+
+        {console.log(tableData)}
+        {tableData.map((item, index) => (
+          <TableRow key={index}>
+            <TableCell>
+              <Label>{item.ledger}</Label>
+            </TableCell>
+            <TableCell>
+              <Label>{item.companyCode}</Label>
+            </TableCell>
+            <TableCell>
+              <Label>{item.account}</Label>
+            </TableCell>
+            <TableCell>
+              <Label>{item.postingDate}</Label>
+            </TableCell>
+            <TableCell>
+              <Label>{item.amount}</Label>
+            </TableCell>
+          </TableRow>
+        ))}
+      </Table>
     </ThemeProvider>
   );
 }
